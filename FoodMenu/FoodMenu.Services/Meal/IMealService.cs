94e9e6ffeabd05c254d9cf2a5bccb67b62fde64a
Ugnius Saulevičius <ugnius.saulevicius@gmail.com>
2023-02-01 23:38:55 +0200

    feat: add filtering by same category

--- FoodMenu/FoodMenu.Services/Meal/IMealService.cs
+++ FoodMenu/FoodMenu.Services/Meal/IMealService.cs
@@ -5,5 +5,6 @@ namespace FoodMenu.Services
     public interface IMealService
     {
         Task<Meal?> GetMeal(string name);
+        Task<IList<string>> GetMealsByCategory(string category, int count);
     }
 }
